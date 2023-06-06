a2e7dd4f03d7a63d153ea00815f28c781bbdf3a6
Dennismarangu <dennismutumamarangu@gmail.com>
2023-06-06 20:21:25 +0300

    Completed assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    actors.map(&:full_name)
+  end
+
+  def characters
+    Character.where(show_id: id)
+  end
+
+  def network
+    Network.find(network_id)
+  end
 end
